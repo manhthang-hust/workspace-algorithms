6da98bc7f86c092bdfb54e9ae11cb5978da34df0
Nguyễn Mạnh Thắng <80304974+manhthang-hust@users.noreply.github.com>
2021-12-22 23:06:52 +0700

    Update 05NURSE.cpp

--- TTUD_Codeforces/05NURSE.cpp
+++ TTUD_Codeforces/05NURSE.cpp
@@ -1,13 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, k1, k2;
+#define HNT  ios_base::sync_with_stdio(false);    cin.tie(0);
 
-void input(){
-    cin >> n >> k1 >> k2;
+long long N, K1, K2, cnt = 0;
+
+void TRY(int v){
+	for(int k = K1; k <= K2; k++){
+		v = v + k;
+		if(v == N || v == (N-1)){
+			cnt++;
+		}
+		else{
+			if(v + K1 <= N) TRY(v+1);
+		}
+		v = v - k;
+	}
 }
 
 int main(){
-    input();
-
-    return 0;
+    HNT;
+	cin >> N >> K1 >> K2;
+	TRY(0);
+	TRY(1);
+	cout << cnt;
 }
